7403b265aa7ebfa65c606c3b54a055c2fc307c4c
promavto <promavto@ntmp.ru>
2018-02-27 18:23:15 +0300

    Изменил подключение индуктивных датчиков к цифровым портам

---
+++ MAIN/Debug/RussianFonts.c.d
@@ -0,0 +1,2 @@
+C:\Users\Alex\AppData\Local\Temp\VMBuilds\MAIN\arduino_due_x_dbg\Debug\RussianFonts.c.o: \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\MAIN\arduino_due_x_dbg\Debug\RussianFonts.c
---
+++ MAIN/Debug/Various_Symbols_16x32.c.d
@@ -0,0 +1,2 @@
+C:\Users\Alex\AppData\Local\Temp\VMBuilds\MAIN\arduino_due_x_dbg\Debug\Various_Symbols_16x32.c.o: \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\MAIN\arduino_due_x_dbg\Debug\Various_Symbols_16x32.c
