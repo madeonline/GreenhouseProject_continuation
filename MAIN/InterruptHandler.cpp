788de7e8979fa56b007a033f6a599686aa176742
Porokhnya Dmitry <spywarrior@gmail.com>
2018-03-20 12:16:09 +0300

    Перенёс реакцию на срабатывание прерываний в класс прерываний

--- MAIN/InterruptHandler.cpp
+++ MAIN/InterruptHandler.cpp
@@ -81,18 +81,30 @@ void InterruptHandlerClass::update()
     if(copyList1.size() > 1)
     {
       DBGLN("INTERRUPT #1 HAS SERIES OF DATA!");
+
+      // зажигаем светодиод "ТЕСТ"
+      InfoDiodes.test();
+        
        //TODO: здесь мы можем обрабатывать список сами - в нём ЕСТЬ данные !!!
     }
     
     if(copyList2.size() > 1)
     {
       DBGLN("INTERRUPT #2 HAS SERIES OF DATA!");
+
+      // зажигаем светодиод "ТЕСТ"
+      InfoDiodes.test();
+       
        //TODO: здесь мы можем обрабатывать список сами - в нём ЕСТЬ данные !!!
     }
     
     if(copyList3.size() > 1)
     {
       DBGLN("INTERRUPT #3 HAS SERIES OF DATA!");
+
+      // зажигаем светодиод "ТЕСТ"
+      InfoDiodes.test();
+       
        //TODO: здесь мы можем обрабатывать список сами - в нём ЕСТЬ данные !!!
     }
     
